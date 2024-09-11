f9fa320bc2f65b0927d938db4c08deea08290636
sedra_alkhateeb <sssedraalllkateb@gmail.com>
2024-09-11 13:52:24 +0400

    sedra

---
+++ lib/domain/usecase/insert_all_hospitals_sql_usecase .dart
@@ -0,0 +1,17 @@
+import 'package:dartz/dartz.dart';
+import 'package:domina_app/data/network/failure.dart';
+import 'package:domina_app/domain/models/models.dart';
+import 'package:domina_app/domain/repostitory/repository_sql.dart';
+import 'package:equatable/equatable.dart';
+
+class InsertAllHospitalsSqlUsecase extends Equatable {
+  final RepositorySql _repositorySql;
+  InsertAllHospitalsSqlUsecase(this._repositorySql);
+  Future<Either<Failure, Null>> execute(List<DoctorModel> doctorModel) async {
+    return await _repositorySql.insertHospital(doctorModel);
+  }
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [_repositorySql];
+}
