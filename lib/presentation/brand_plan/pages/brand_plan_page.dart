91fce8050be3a966925c6a8d9fa0abad7a4fc6bc
sedra_alkhateeb <sssedraalllkateb@gmail.com>
2024-10-20 12:08:31 +0400

    shhd

--- lib/presentation/brand_plan/pages/brand_plan_page.dart
+++ lib/presentation/brand_plan/pages/brand_plan_page.dart
@@ -1,9 +1,7 @@
-import 'package:domina_app/presentation/brand_plan/bloc/brand_plan_bloc.dart';
 import 'package:domina_app/presentation/brand_plan/widget/brand_plan_active_page.dart';
 import 'package:domina_app/presentation/brand_plan/widget/brand_plan_other_page.dart';
 import 'package:domina_app/presentation/resources/color_manager.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 class BrandPlanPage extends StatelessWidget {
   const BrandPlanPage({super.key});
