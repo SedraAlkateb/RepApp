e5a4ab888c7df26afe9f44ba1c5d164d25458fba
Sedra Alkhateeb <sssedraalllkateb@gmail.com>
2025-03-20 10:50:02 +0400

    dnfgpdsngngnsepg Gr ng[

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -4,7 +4,7 @@ import 'package:domina_app/presentation/Recipes/bloc/recipes_brand_bloc.dart';
 import 'package:domina_app/presentation/async/bloc/async_bloc.dart';
 import 'package:domina_app/presentation/brand_plan/bloc/brand_plan_bloc.dart';
 import 'package:domina_app/presentation/delete/bloc/delete_bloc.dart';
-import 'package:domina_app/presentation/senior/edit_brand/bloc/edit_brand_plan_bloc.dart';
+import 'package:domina_app/presentation/senior/edit_brand_plan/bloc/edit_brand_plan_bloc.dart';
 import 'package:domina_app/presentation/senior/future_rep/bloc/future_rep_bloc.dart';
 import 'package:domina_app/presentation/senior/manage_future/bloc/manage_future_bloc.dart';
 import 'package:domina_app/presentation/senior/places/bloc/senior_reps_bloc.dart';
