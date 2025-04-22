a638fc1e27fc314d74f4fbb9ce14f8d9e8e72c35
Sedra Alkhateeb <sssedraalllkateb@gmail.com>
2025-04-22 16:23:59 +0400

    merg

--- lib/presentation/Recipes/pages/update_recipes_hospital.dart
+++ lib/presentation/Recipes/pages/update_recipes_hospital.dart
@@ -853,66 +853,33 @@ class _UpdateRecipesHospitalState extends State<UpdateRecipesHospital> {
                               state.failure.code);
                         }
                       },
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          ElevatedButton(
-                            onPressed: () {
-                              if (_formKey.currentState!.validate()) {
-                                BlocProvider.of<RecipesBrandBloc>(context).add(
-                                    UpdateReciSHospitalEvent(
-                                        widget.HospitalId,
-                                        _doctorSpController.text,
-                                        firstNoteController.text,
-                                        _secondNoteController.text,
-                                        _addressController.text,
-                                        _connectController.text,
-                                        _specialNotesController.text,
-                                        widget.recipeId));
-                              } else {
-                                ScaffoldMessenger.of(context).showSnackBar(
-                                  SnackBar(
-                                    content:
-                                        Text('يرجى تعبئة جميع الحقول المطلوبة'),
-                                    backgroundColor:
-                                        ColorManager.secondaryColor,
-                                  ),
-                                );
-                              }
-                            },
-                            child: Text(
-                              "إرسال",
-                            ),
-                          ),
-                          ElevatedButton(
-                            onPressed: () {
-                              if (_formKey.currentState!.validate()) {
-                                BlocProvider.of<RecipesBrandBloc>(context)
-                                    .add(InsertReciHospitalEvent(
-                                  widget.HospitalId,
-                                  _doctorSpController.text,
-                                  firstNoteController.text,
-                                  _secondNoteController.text,
-                                  _addressController.text,
-                                  _connectController.text,
-                                  _specialNotesController.text,
-                                ));
-                              } else {
-                                ScaffoldMessenger.of(context).showSnackBar(
-                                  SnackBar(
-                                    content:
-                                        Text('يرجى تعبئة جميع الحقول المطلوبة'),
-                                    backgroundColor:
-                                        ColorManager.secondaryColor,
-                                  ),
-                                );
-                              }
-                            },
-                            child: Text(
-                              "إرسال",
-                            ),
-                          ),
-                        ],
+                      child:   ElevatedButton(
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            BlocProvider.of<RecipesBrandBloc>(context).add(
+                                UpdateReciSHospitalEvent(
+                                    widget.HospitalId,
+                                    _doctorSpController.text,
+                                    firstNoteController.text,
+                                    _secondNoteController.text,
+                                    _addressController.text,
+                                    _connectController.text,
+                                    _specialNotesController.text,
+                                    widget.recipeId));
+                          } else {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content:
+                                Text('يرجى تعبئة جميع الحقول المطلوبة'),
+                                backgroundColor:
+                                ColorManager.secondaryColor,
+                              ),
+                            );
+                          }
+                        },
+                        child: Text(
+                          "إرسال",
+                        ),
                       ),
                     ),
                   ],
